eacd1a3a7c958f01a045d7dc8edcd3b52fc970d2
Pratim Ugale <pratim.ugale@gmail.com>
2018-03-17 16:48:51 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cp .arduino_mk ~/
+cp -r .arduino_mk ~/
 mkdir ~/ardbin/
 cp ard-upload ~/ardbin
 cp make-uno ~/ardbin
 chmod +x ~/ardbin/ard-upload
 chmod +x ~/ardbin/make-uno
 echo "export PATH=\$PATH:~/ardbin" >> ~/.bashrc
-
+. ~/.bashrc
