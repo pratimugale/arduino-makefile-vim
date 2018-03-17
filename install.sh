31a564a2556f9974525c9387ecfcdb6a8a330407
Pratim Ugale <pratim.ugale@gmail.com>
2018-03-17 19:54:01 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,7 @@ cp makefile-uno ~/ardbin
 chmod +x ~/ardbin/ard-upload
 chmod +x ~/ardbin/makefile-uno
 echo "export PATH=\$PATH:~/ardbin" >> ~/.bashrc
-cd
-. ~/.bashrc
+
+#The following commands seem to have no effect and hence have to be done manually
+#cd
+#. ~/.bashrc
